469d87170adcf13ba4a986d539db6e9da98ff88c
fred <fredrischter@gmail.com>
2018-11-05 21:05:53 -0300

    project basic structure

---
+++ src/main/java/com/creditsuisse/eventcontrol/EventcontrolApplication.java
@@ -0,0 +1,12 @@
+package com.creditsuisse.eventcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventcontrolApplication.class, args);
+	}
+}
